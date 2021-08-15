51e827cc6ae3626e2b518244a99e5ca8ffc9f69a
Spencer <spencer676@hotmail.com>
2021-08-15 15:48:12 +0200

    using fetch to get word list

--- src/WordField.js
+++ src/WordField.js
@@ -1,26 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function WordField() {
-  const [data, setData] = useState([]);
-  const GetApi = () => {
-    fetch("http://localhost:8000/wordPairList")
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        setData(json);
-      });
-  };
+  
+      function WordField() {
+        // const [data, setData] = useState( {wordPairList: []});
+        const [data, setData] = useState(null);
+        const [isPending, setIsPending] = useState(true);
+
+        // const GetApi = null;
+        // const randomWordPair = {};
+        // const GetApi = null;
+
+        useEffect(() => {
+          setTimeout(() =>{
+            // GetApi = () => {
+              fetch("http://localhost:9080/api/EN/list")
+                .then((res) => res.json())
+                .then((json) => {
+                  console.log(json);
+                  setData(json);
+                  setIsPending(false);
+                });
+                console.log("data: " + data);
+                console.log("setData: " + setData);
+
+                // const array = data.wordPairList;
+                // const randomNumber = Math.floor(Math.random() * array.length);
+                // randomWordPair = array[randomNumber];
+                // console.log(array)
+                // console.log(randomWordPair);
+            // };
+          }, 5000);
+        },[]);
+
+
+       
+
+// const array = data.wordPairList;
+// const randomNumber = Math.floor(Math.random() * array.length);
+// const randomWordPair = array[randomNumber];
+// console.log(array)
+// console.log(randomWordPair);
 
   return (
+    
     <div className="word">
-      <button onClick={GetApi}>Submit</button>
+      {/* {isPending && <button onClick={GetApi}>Submit</button>} */}
+      
       <h3 className="wordfield-text">Woord dat vertaald moet worden</h3>
       <div className="wordfield">
-        {/* <h3>{JSON.stringify(data)}</h3> */}
         <div>
-          {data.map((item) => (
+        {isPending && <div>Loading...</div>}
+          {data &&<h3>{data.wordPairList.question}</h3>}
+          {/* {randomWordPair &&<h3>{randomWordPair.question}</h3>} */}
+          {/* <h3>{data.wordPairList[0].question}</h3> */}
+          {/* <h3>{data.wordPairList[0].question}</h3> */}
+
+          {/* {data.wordPairList.map((item) => (
             <h3>{item.question}</h3>
-          ))}
+            
+          ))} */}
         </div>
       </div>
     </div>
